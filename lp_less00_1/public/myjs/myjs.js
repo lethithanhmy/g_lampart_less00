f8adc1ff4e7ef29112a80ea5b28f5ce2cdec2bb4
Trần Minh Nhựt <comboyin1@gmail.com>
2015-10-25 21:20:18 +0700

    a

---
+++ lp_less00_1/public/myjs/myjs.js
@@ -0,0 +1,13 @@
+/* ------------------------ document redy -----------------------*/
+
+/* ---------------document ready end----------------- */
+
+jQuery(document).ready(function () {
+
+	var abc = $( "ul.sf-menu" )[0];
+	var as = $( 'li' , abc );
+	var asdasd = $("ul.sf-menu").parents('div')[0];
+	
+	
+	
+});
